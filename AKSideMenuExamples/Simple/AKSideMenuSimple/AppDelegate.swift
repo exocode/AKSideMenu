9d0a9fce81aef73a015e45e24a6219e51a39a2f1
Jan <mail@mediatainment-productions.com>
2017-06-27 07:48:35 +0530

    issue demo

---
+++ AKSideMenuExamples/Simple/AKSideMenuSimple/AppDelegate.swift
@@ -0,0 +1,82 @@
+//
+//  AppDelegate.swift
+//  AKSideMenu
+//
+//  Created by Diogo Autilio on 6/3/16.
+//  Copyright © 2016 AnyKey Entertainment. All rights reserved.
+//
+
+import UIKit
+import AKSideMenu
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, AKSideMenuDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+
+        // Create content and menu controllers
+        let navigationController = UINavigationController(rootViewController: FirstViewController())
+        let leftMenuViewController = LeftMenuViewController()
+        let rightMenuViewController = RightMenuViewController()
+
+        // Create side menu controller
+        let sideMenuViewController: AKSideMenu = AKSideMenu(contentViewController: navigationController, leftMenuViewController: leftMenuViewController, rightMenuViewController: rightMenuViewController)
+
+        sideMenuViewController.backgroundImage = UIImage(named: "Stars")!
+        sideMenuViewController.menuPreferredStatusBarStyle = .lightContent
+        sideMenuViewController.delegate = self
+        sideMenuViewController.contentViewShadowColor = .black
+        sideMenuViewController.contentViewShadowOffset = CGSize(width: 0, height: 0)
+        sideMenuViewController.contentViewShadowOpacity = 0.6
+        sideMenuViewController.contentViewShadowRadius = 12
+        sideMenuViewController.contentViewShadowEnabled = true
+        self.window!.rootViewController = sideMenuViewController
+
+        self.window!.backgroundColor = UIColor.white
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidvar timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    // MARK: - <AKSideMenuDelegate>
+
+    open func sideMenu(_ sideMenu: AKSideMenu, willShowMenuViewController menuViewController: UIViewController) {
+        print("willShowMenuViewController")
+    }
+
+    open func sideMenu(_ sideMenu: AKSideMenu, didShowMenuViewController menuViewController: UIViewController) {
+        print("didShowMenuViewController")
+    }
+
+    open func sideMenu(_ sideMenu: AKSideMenu, willHideMenuViewController menuViewController: UIViewController) {
+        print("willHideMenuViewController")
+    }
+
+    open func sideMenu(_ sideMenu: AKSideMenu, didHideMenuViewController menuViewController: UIViewController) {
+        print("didHideMenuViewController")
+    }
+}
